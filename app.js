803ace932c6d49e524ff2c6329e3100a3e639e27
Dhirendra Singh <dhirendra.singh@AD.SAASFOCUS.COM>
2019-02-22 15:04:37 +0530

    added aap

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+http = require('http'),
+request = require('request'),
+bodyParser = require('body-parser'),
+errorHandler = require('express-error-handler'),
+app = express();
+
+var pg = require('pg');  
+//You can run command "heroku config" to see what is Database URL from Heroku belt
+
+var conString = process.env.DATABASE_URL || "postgres://postgres:Welcome123@localhost:5432/postgres";
+var client = new pg.Client(conString);
+
+client.connect();
+
+var query = client.query("select * from salesforce.contact"); 
+console.log(query); 
+
+
+
+
+app.set('port', process.env.PORT || 3001);
+
+app.use(express.static(__dirname + '/client')); 
+app.use(errorHandler());
+app.listen(app.get('port'), function () {
+    console.log('Server listening on port ' + app.get('port'));
+});
