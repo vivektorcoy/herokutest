f345125945cc73bf896739957994da6253fd595d
Vivek Kumar <vivek.kumar@AD.SAASFOCUS.COM>
2019-02-25 14:22:56 +0530

    api first

--- app.js
+++ app.js
@@ -4,48 +4,43 @@ request = require('request'),
 bodyParser = require('body-parser'),
 errorHandler = require('express-error-handler'),
 app = express();
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 
-var pg = require('pg');  
-//You can run command "heroku config" to see what is Database URL from Heroku belt
+var pg = require('pg');
 
-//var conString = process.env.DATABASE_URL || "postgres://postgres:Welcome123@localhost:5432/postgres";
-//var client = new pg.Client(conString);
 var client = new pg.Client({
-    connectionString: process.env.DATABASE_URL || "postgres://postgres:Welcome123@localhost:5432/postgres",
+    connectionString: process.env.DATABASE_URL || 'postgres://tnvkutdzqbdptn:38dd0a8b1255d1a30f01f94bc771e1095e51956398f1c755e5d510fec2e26340@ec2-107-20-167-11.compute-1.amazonaws.com',
     ssl: true,
   });
+
 client.connect();
 
-app.set('port', process.env.PORT || 3001);
-app.get('/fetchContacts', function(request, response) {
-    //response.send('Hello World!');
-    //var query = client.query("select * from salesforce.contact;");
+ app.post('/updateContact',function(req,res)
+ {
+     var lead_id = req.body.lead_id;
+     var opp_id = req.body.opp_id;
+     var con_id = req.body.con_id;
+     var LoanExt_id = req.body.LoanExt_id;
+     var mobile_number = req.body.mobile_number;
+     var device_ID = req.body.device_ID;
+     var mpin = req.body.mpin;
 
-    client.query('select sfId, LastName, Email, MobilePhone from salesforce.contact;', (err, res) => {
-        if(err) throw err;
-        var fetchedContactList = [];
-        for (let row of res.rows) {
-            fetchedContactList.push(row);
+     client.query('UPDATE salesforce.contact SET IVL_Device_Id__c = device_ID, IVL_MPIN__c=mpin  WHERE ID = con_id RETURNING sfid',(err, res) => {
+        if(err)
+        {
+            throw err;
         }
-        response.send(JSON.stringify(fetchedContactList));
-
-        //client.end();
-        response.end();
+        else
+        {
+            res.send('Record updated successfully');
+        }
     });
+ });
 
-    /*client.query('UPDATE salesforce.contact SET Email = \'gg@kk.com\' WHERE LastName = \'Applicant1\' RETURNING sfid;',(err, res) => {
-        if(err) throw err;
-        var toUpdateContactList = [];
-        for(let row of res.rows){
-            toUpdateContactList.push(row);
-        }
-        response.send(JSON.stringify(toUpdateContactList));
-        client.end();
-    });*/
-
-});
-
-app.use(errorHandler());
-app.listen(app.get('port'), function () {
+ app.set('port', process.env.PORT || 3001);
+ app.listen(app.get('port'), function () {
     console.log('Server listening on port ' + app.get('port'));
 });
