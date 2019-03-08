abd0d160dfafc291860974c8dba55bb5e5ff83a2
Vivek Kumar <vivek.kumar@AD.SAASFOCUS.COM>
2019-03-08 12:53:02 +0530

    ff

--- MobileRegWebservice.js
+++ MobileRegWebservice.js
@@ -45,15 +45,14 @@ module.exports = {
 					res.send(JSON.stringify(apiresponse));
 
 					var contactId = req.body.con_id;
-					var requestBody = req.body;
+					var requestBody = JSON.stringify(req.body);
 					var jsonApiResponse = JSON.stringify(apiresponse);
 
 					var param1 = 'IVL_Error_Log__c';
-					var param2 = 'IVL_API_Name__c=mobileregistration,IVL_Is_Error_Exception__c=true,IVL_Contact__c='+contactId+',IVL_Request__c='+requestBody+',IVL_Response__c='+jsonApiResponse+',IVL_Type__c= inbound';
-					var queryData="SELECT INSERT_OPERATION(\'"+param1+"\',\'"+param2+"\')";
-					console.log("query data :::: "+queryData);
+					var param2 = 'IVL_API_Name__c=mobileregistration,IVL_Is_Error_Exception__c=false,IVL_Request__c= test_request,IVL_Response__c= test_response,IVL_Type__c= inbound';
+					var queryData="SELECT INSERT_OPERATION("+param1+","+param2+")";
+					console.log("query data else :::: "+queryData);
 					//client.query('INSERT INTO salesforce.IVL_Error_Log__c (IVL_API_Name__c,IVL_Is_Error_Exception__c,IVL_Contact__c,IVL_Request__c,IVL_Response__c,IVL_Type__c) VALUES(\'mobileregistration\',false,($1),($2),($3),\'inbound\')',
-					//client.query("SELECT INSERT_OPERATION(IVL_Error_Log__c,IVL_API_Name__c=mobileregistration,IVL_Is_Error_Exception__c=false,IVL_Contact__c=($1),IVL_Request__c=($2),IVL_Response__c=($3),IVL_Type__c= inbound)",
 					client.query(queryData,
 					function(err, result)
 					{
